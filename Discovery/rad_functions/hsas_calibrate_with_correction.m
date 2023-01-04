4393a8d39a21ea990d4fe7d58e1536bce72f6cd7
Tom Jordan <tjor@pmpc1460.npm.ac.uk>
2023-01-04 14:48:35 +0000

    FICE version of cal with corr

--- Discovery/rad_functions/hsas_calibrate_with_correction.m
+++ Discovery/rad_functions/hsas_calibrate_with_correction.m
@@ -1,4 +1,4 @@
-function cal = hsas_calibrate_with_correction(sensor_id, wl, L_CountsLightDat, L_CalDarkDat, a, ic, it_1, it_2, rad_sn, sn_rad, non_linearity_coeff, D_SL)
+function cal = hsas_calibrate_with_correction(sensor_id, wl, L_CountsLightDat, L_CalDarkDat, a, ic, it_1, it_2, rad_sn, sn_rad, non_linearity_coeff, D_SL,FLAG_NON_LINEARITY,FLAG_STRAY_LIGHT)
 % cal = hsas_calibrate(L_CountsLightDat, L_CalDarkDat, a, ic, it_1, it_2)
 #
 # cal = (L_CountsLightDat - L_CalDarkDat).*a.*ic.*it_1./it_2
@@ -16,16 +16,28 @@ function cal = hsas_calibrate_with_correction(sensor_id, wl, L_CountsLightDat, L
 
 
 ################################# Correction ######################
+
+
 data = (L_CountsLightDat - L_CalDarkDat);
-#---- nonlinear correction
-data = correct_non_linearity_at_Cal(rad_sn, sn_rad, sensor_id, non_linearity_coeff,data,wl);
+
+
+
+
+#---- nonlinear correction - #
+if FLAG_NON_LINEARITY == 1
+	data = correct_non_linearity_at_Cal_FICE(rad_sn, sn_rad, sensor_id, non_linearity_coeff,data,wl);
+endif
+
+
 
 #---Stray Light Correction
-data = hsas_straylight_correct_at_Cal(sensor_id, wl, sn_rad, D_SL, data);
+if FLAG_STRAY_LIGHT == 1
+	data = hsas_straylight_correct_at_Cal(sensor_id, wl, sn_rad, D_SL, data);
+endif
 
-#--- Calibration	
+
 cal = data.*a.*ic.*it_1./it_2;
-	
+
 	
 	
 endfunction
