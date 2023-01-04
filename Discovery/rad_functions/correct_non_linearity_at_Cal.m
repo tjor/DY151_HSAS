552ef53bb72f9cbef9984dd4fbaf43afcf7e406b
Tom Jordan <tjor@pmpc1460.npm.ac.uk>
2023-01-04 15:01:15 +0000

    FICE versions of NL and SL correction functions. NL has been tested, SL requires fixings?

--- Discovery/rad_functions/correct_non_linearity_at_Cal.m
+++ Discovery/rad_functions/correct_non_linearity_at_Cal.m
@@ -1,4 +1,7 @@
-function [out] = correct_non_linearity_at_Cal(rad_sn,sn_rad,sn,coeff,in,wv)
+function [out] = correct_non_linearity_at_Cal_FICE(rad_sn,sn_rad,sn,coeff,in,wv)
+  # This is a modified version of correct_non_linearity_at_Cal for FICE 2022, accomodating new data format from tartu.
+  # alpha is calculated within the function using S1 and S2 vectors and t1 and t2 integration times (refer to radcal_recipies.txt).
+
   disp("Non-linearity correction... Wait!")
   data = in;
   sn =str2num(sn);
@@ -12,8 +15,25 @@ function [out] = correct_non_linearity_at_Cal(rad_sn,sn_rad,sn,coeff,in,wv)
     coeff0=coeff.coeff_ES;
   endif
 
-  alf = interp1(coeff0(:,1), coeff0(:,2), wv)';
-  err = 1-alf.*data;
+
+  # extract data from NL coeff matrix - refer to radcal_recipies.txt 
+  wv_nl = coeff0(2:end,2); # nl subscript indicates wv vector is different length from nl matrix
+  S1 = coeff0(2:end,7);
+  S2 = coeff0(2:end,9);
+  t1 = coeff0(1,7);
+  t2 = coeff0(1,9);
+  
+  # compute alpha
+  K = t1/(t2 + t1);
+  S12 = (1 + K)*S1 - K*S2;
+  alpha_nl = (S1 - S12)./S12.^2; 
+
+  # previous steps are from correct_non_linearity_at_Cal
+  alpha = interp1(wv_nl, alpha_nl, wv,'extrap')'; # adjusts to length of cal file
+  err = ones(size(data)) - alpha.*data; 
+
   data_corrected = data.*err;
   out = data_corrected;
+
+  
 endfunction
--- Discovery/rad_functions/hsas_straylight_correct_at_Cal.m
+++ Discovery/rad_functions/hsas_straylight_correct_at_Cal.m
@@ -1,23 +1,30 @@
 function data = hsas_straylight_correct_at_Cal(sensor_id,wl,sn_rad, D, data)
 input_parameters_hsas;
 
+# tjor-  matrix D is the straylight distribution matrix. For FICE, this has already been converted from the linear spread function in hsas calibrabate.m when the SL files are read. See Zong 2006 for detials of the method/
+
 VBS = false;
 
-I = eye(length(wv),length(wv));
+I = eye(length(D),length(D));  
 
-#----Correction for Es
-A = I +D;
+A = I + D;
+C = inv(A); # straylight correction matrix
+ 
+
 disp(['** Straylight correction for ', sn_rad.(sensor_id), ' wait!'])
-
 
 out = ones(size(data));
 for i = 1:length(data(:,1))
- correct_data = inv(A) * [data(i,:),0]';
- out(i,:) = correct_data(1:end-1);
+ 	correct_data = C * [data(i,:)]'; 
+ 	out(i,:) = correct_data(1:end);
 end
 
 data = out;
 
+
 endfunction
+
+
+
 
 
